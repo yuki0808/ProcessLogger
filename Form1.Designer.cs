1d3156aa345091957cb96d65b26622647e208abe
Yuki S <31408900+yuki0808@users.noreply.github.com>
2017-09-30 09:08:29 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+namespace ProcessLogger
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.refreshB = new System.Windows.Forms.Button();
+      this.stopB = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.secLB = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.processDGV = new System.Windows.Forms.DataGridView();
+      this.dgvCB = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+      this.dgvPID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.dgvPName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.dgvUsrName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      ((System.ComponentModel.ISupportInitialize)(this.processDGV)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.button1.Location = new System.Drawing.Point(37, 479);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(117, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "開始";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+      this.label1.Location = new System.Drawing.Point(17, 16);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(74, 24);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "プロセス一覧";
+      // 
+      // refreshB
+      // 
+      this.refreshB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.refreshB.Location = new System.Drawing.Point(344, 16);
+      this.refreshB.Name = "refreshB";
+      this.refreshB.Size = new System.Drawing.Size(56, 23);
+      this.refreshB.TabIndex = 0;
+      this.refreshB.Text = "更新";
+      this.refreshB.UseVisualStyleBackColor = true;
+      this.refreshB.Click += new System.EventHandler(this.refreshB_Click);
+      // 
+      // stopB
+      // 
+      this.stopB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.stopB.Enabled = false;
+      this.stopB.Location = new System.Drawing.Point(263, 479);
+      this.stopB.Name = "stopB";
+      this.stopB.Size = new System.Drawing.Size(117, 23);
+      this.stopB.TabIndex = 0;
+      this.stopB.Text = "停止";
+      this.stopB.UseVisualStyleBackColor = true;
+      this.stopB.Click += new System.EventHandler(this.stopB_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBox1.Location = new System.Drawing.Point(329, 445);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(51, 19);
+      this.textBox1.TabIndex = 4;
+      this.textBox1.Text = "3";
+      this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+      this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+      this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+      this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+      // 
+      // secLB
+      // 
+      this.secLB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.secLB.AutoSize = true;
+      this.secLB.Location = new System.Drawing.Point(264, 448);
+      this.secLB.Name = "secLB";
+      this.secLB.Size = new System.Drawing.Size(59, 12);
+      this.secLB.TabIndex = 2;
+      this.secLB.Text = "出力間隔：";
+      // 
+      // label2
+      // 
+      this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(386, 448);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(17, 12);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "秒";
+      // 
+      // processDGV
+      // 
+      this.processDGV.AllowUserToAddRows = false;
+      this.processDGV.AllowUserToDeleteRows = false;
+      this.processDGV.AllowUserToResizeRows = false;
+      this.processDGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.processDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.processDGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvCB,
+            this.dgvPID,
+            this.dgvPName,
+            this.dgvUsrName});
+      this.processDGV.Location = new System.Drawing.Point(21, 47);
+      this.processDGV.Name = "processDGV";
+      this.processDGV.RowHeadersVisible = false;
+      this.processDGV.RowTemplate.Height = 21;
+      this.processDGV.Size = new System.Drawing.Size(379, 392);
+      this.processDGV.TabIndex = 5;
+      // 
+      // dgvCB
+      // 
+      this.dgvCB.HeaderText = "";
+      //this.dgvCB.Name = "";
+      this.dgvCB.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+      this.dgvCB.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+      this.dgvCB.Width = 30;
+      // 
+      // dgvPID
+      // 
+      this.dgvPID.HeaderText = "PID";
+      this.dgvPID.Name = "dgvPID";
+      this.dgvPID.ReadOnly = true;
+      this.dgvPID.Width = 80;
+      // 
+      // dgvPName
+      // 
+      this.dgvPName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.dgvPName.HeaderText = "プロセス名";
+      this.dgvPName.Name = "dgvPName";
+      this.dgvPName.ReadOnly = true;
+      // 
+      // dgvUsrName
+      // 
+      this.dgvUsrName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+      this.dgvUsrName.HeaderText = "ユーザー名";
+      this.dgvUsrName.Name = "dgvUsrName";
+      this.dgvUsrName.ReadOnly = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(417, 514);
+      this.Controls.Add(this.processDGV);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.secLB);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.refreshB);
+      this.Controls.Add(this.stopB);
+      this.Controls.Add(this.button1);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "ProcessLogger Ver.1.0";
+      this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+      ((System.ComponentModel.ISupportInitialize)(this.processDGV)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button refreshB;
+    private System.Windows.Forms.Button stopB;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label secLB;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.DataGridView processDGV;
+    private DataGridViewCheckBoxColumn dgvCB;
+    private DataGridViewTextBoxColumn dgvPID;
+    private DataGridViewTextBoxColumn dgvPName;
+    private DataGridViewTextBoxColumn dgvUsrName;
+
+  }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProcessLogger
+{
+
+  public partial class Form1 : Form
+  {
+
+    private DatagridViewCheckBoxHeaderCell2 cbHeader = new DatagridViewCheckBoxHeaderCell2();
+    ArrayList pList = new ArrayList();
+    static Boolean loopFlg = true;
+    //Thread thread;
+    private string tempSec = "3";
+
+
+    public Form1()
+    {
+      InitializeComponent();
+      pList = getProcessList();
+
+      generateProcessListDGV();
+      this.dgvCB.HeaderCell = cbHeader;
+      cbHeader.OnCheckBoxClicked += new CheckBoxClickedHandler(cbHeader_OnCheckBoxClicked);
+
+      //foreach( string token in pList ){
+      //  psCB.Items.Add(token);
+      //}
+    }
+
+    private void cbHeader_OnCheckBoxClicked(bool state)
+    {
+      Boolean checkBool = false;
+
+      if (cbHeader.isChecked())
+      {
+        checkBool = true;
+        
+      }
+      else {
+        checkBool = false;
+      }
+
+      foreach (DataGridViewRow dgvr in processDGV.Rows)
+      {
+        dgvr.Cells[0].Value = checkBool;
+      }
+    }
+
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+
+      //if(psCB.Text == ""){
+      //  return;
+      //}
+
+      ArrayList pList = getCheckedPIDList();
+
+      if(pList.Count == 0){
+        return;
+      }
+
+      button1.Enabled = false;
+      stopB.Enabled = true;
+      string timeInterval = textBox1.Text;
+
+      foreach(String token in pList){
+
+        string pId = token;
+        String[] param = { pId, timeInterval };
+
+        Thread thread = new Thread(new ParameterizedThreadStart(loggingProcessInfo));
+        thread.Start(param);
+      }
+    }
+
+    private ArrayList getCheckedPIDList() {
+      ArrayList result = new ArrayList();
+
+      foreach (DataGridViewRow dgvr in processDGV.Rows)
+      {
+        if ((Boolean)dgvr.Cells[0].Value)
+        {
+          result.Add(dgvr.Cells[1].Value.ToString());
+        }
+      }
+      return result;
+    }
+
+
+    private void loggingProcessInfo(Object obj)
+    {
+
+      //string pName = (string)obj;
+      string[] param = (String[])obj;
+      //string pName = param[0];
+      loopFlg = true;
+
+      //ProcessID版
+      int pId = int.Parse(param[0]);
+      int timeInterval = int.Parse(param[1])*1000;
+      //int pid = int.Parse(param[2]);
+
+      while (loopFlg)
+      {
+        //Process名版
+        System.Diagnostics.Process ps = System.Diagnostics.Process.GetProcessById(pId);
+       
+        //ProcessID版 Start
+        //System.Diagnostics.Process ps = System.Diagnostics.Process.GetProcessById(pid);
+        //WriteTraceLog(ps.Id + "," + ps.ProcessName + "," + ps.TotalProcessorTime + "," + ps.PrivateMemorySize64);
+        
+        //ProcessID版 END
+        Thread.Sleep(timeInterval);
+
+        //Console.WriteLine(p.ProcessName + "," + p.TotalProcessorTime + "," + p.WorkingSet64);
+        WriteTraceLog(ps.Id + "," + ps.ProcessName + "," + ps.TotalProcessorTime + "," + ps.PrivateMemorySize64, ps.Id.ToString());
+
+      }
+
+
+    }
+
+    private static void WriteTraceLog(String msg, String pId)
+    {
+      WriteTraceLog(msg, pId, null);
+    }
+
+
+    private static void WriteTraceLog(String msg, String logSuffix, Exception ex)
+    {
+      try
+      {
+        // ログフォルダ名作成
+        DateTime dt = DateTime.Now;
+        String logFolder = System.AppDomain.CurrentDomain.BaseDirectory + "Log";
+
+        // ログフォルダ名作成
+        System.IO.Directory.CreateDirectory(logFolder);
+
+        // ログファイル名作成
+        String logFile = logFolder + "\\Process_" + logSuffix +".log";
+
+        // 翌日分のログファイル削除(１ヶ月分のログファイルしか保存しないようにするため)
+        //String logNext = logFolder + "\\TraceLog" + dt.AddDays(1).ToString("dd") + ".log";
+        //System.IO.File.Delete(logNext);
+
+        // ログ出力文字列作成
+        String logStr;
+        logStr = dt.ToString("yyyy/MM/dd") + "," + dt.ToString("HH:mm:ss") + "," + msg;
+        if (ex != null)
+        {
+          logStr = logStr + "\n" + ex.ToString();
+        }
+
+        // Shift-JISでログ出力
+        System.IO.StreamWriter sw = null;
+        try
+        {
+          if (!File.Exists(logFile))
+          {
+            sw = new System.IO.StreamWriter(logFile, true,
+                System.Text.Encoding.GetEncoding("Shift-JIS"));
+            sw.WriteLine("Date,Time,ProcessID,ProcessName,TotalProcessorTime,PrivateMemSize");
+          }
+
+          sw = new System.IO.StreamWriter(logFile, true,
+              System.Text.Encoding.GetEncoding("Shift-JIS"));
+          sw.WriteLine(logStr);
+        }
+        catch { }
+        finally
+        {
+          if (sw != null) sw.Close();
+        }
+      }
+      catch { }
+    }
+
+    private ArrayList getProcessList() {
+
+      ArrayList result = new ArrayList();
+
+      System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();
+
+      foreach (System.Diagnostics.Process p in ps)
+      {
+        try
+        {
+          result.Add(p.ProcessName + " " + p.Id); 
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show("エラー: {0}", ex.Message);
+        }
+      }
+
+      result.Sort();
+
+      return result;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void generateProcessListDGV()
+    {
+      ManagementObjectSearcher query = new ManagementObjectSearcher(@"SELECT * FROM Win32_Process");
+      ManagementObjectCollection col = query.Get();
+
+        foreach (ManagementObject mo in col)
+        {
+          string pOwner = "";
+          object[] UserInfo = new object[2];
+          mo.InvokeMethod("GetOwner", UserInfo);
+          pOwner = ((string)UserInfo[0]);
+
+          processDGV.Rows.Add(false, mo["ProcessId"], mo["Name"], pOwner);
+        }
+
+      //foreach (System.Diagnostics.Process p in processes)
+      //{
+      //  //TODO
+      //  ManagementObjectSearcher query = new ManagementObjectSearcher(@"SELECT * FROM Win32_Process Where ProcessId = '" + p.Id + "'");
+      //  ManagementObjectCollection col = query.Get();
+      //  string pOwner = "";
+
+
+      //  //if(col.Count ==1){
+      //  //  foreach (ManagementObject o in col)
+      //  //  {
+      //  //    object[] UserInfo = new object[2];
+      //  //    o.InvokeMethod("GetOwner", UserInfo);
+      //  //    pOwner = ((string)UserInfo[0]);
+      //  //  }
+      //  //}
+
+
+      //  try
+      //  {
+      //    processDGV.Rows.Add(false, p.Id, p.ProcessName, pOwner);
+      //  }
+      //  catch (Exception ex) {
+      //    Console.WriteLine("エラー: {0}", ex.Message);
+      //  }
+
+
+        //try
+        //{
+        //  result.Add(p.ProcessName + " " + p.Id);
+        //}
+        //catch (Exception ex)
+        //{
+        //  Console.WriteLine("エラー: {0}", ex.Message);
+        //  WriteTraceLog("エラー: " + ex.Message);
+        //}
+      //}
+    
+    }
+
+
+
+
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private Process[] getProcesses() {
+
+
+      Process[] result = System.Diagnostics.Process.GetProcesses();
+
+      return result;
+    
+    }
+
+
+    private void refreshB_Click(object sender, EventArgs e)
+    {
+
+      //processDGV.Columns.Clear();
+      do
+      {
+        foreach (DataGridViewRow r in processDGV.Rows)
+        {
+          processDGV.Rows.Remove(r);
+        }
+      } while (processDGV.Rows.Count > 0);
+
+
+      generateProcessListDGV();
+
+      //pList = getProcessList();
+
+      //psCB.Items.Clear();
+
+      //foreach (string token in pList)
+      //{
+      //  psCB.Items.Add(token);
+      //}
+
+    }
+
+    private void stopB_Click(object sender, EventArgs e)
+    {
+      loopFlg = false;
+      button1.Enabled = true;
+      stopB.Enabled = false;
+    }
+
+
+    private string getProcessName(string target) {
+      string result = "";
+
+      int index = target.IndexOf(" ");
+
+
+      result = target.Substring(0, index);
+
+      return result;
+
+    }
+
+    private string getProcessId(string target)
+    {
+      string result = "";
+
+      int index = target.IndexOf(" ");
+
+
+      result = target.Substring(index);
+
+      return result;
+
+    }
+
+    private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b')
+      {
+        e.Handled = true;
+      }
+    }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+      loopFlg = false;
+    }
+
+    private void textBox1_Leave(object sender, EventArgs e)
+    {
+      if (textBox1.Text.Length == 0)
+      {
+        textBox1.Text = this.tempSec;
+      
+      }
+    }
+
+    private void textBox1_Enter(object sender, EventArgs e)
+    {
+      this.tempSec = textBox1.Text;
+    }
+
+
+    public delegate void CheckBoxClickedHandler(bool state);
+    public class DataGridViewCheckBoxHeaderCellEventArgs : EventArgs
+    {
+      bool _bChecked;
+      public DataGridViewCheckBoxHeaderCellEventArgs(bool bChecked)
+      {
+        _bChecked = bChecked;
+      }
+      public bool Checked
+      {
+        get { return _bChecked; }
+      }
+    }
+
+  }
+}
